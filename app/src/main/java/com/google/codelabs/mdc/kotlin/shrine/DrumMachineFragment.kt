b89e4d3b3a1fe7e00291efd62b84283b260b720e
KilliannH <killiannhervagault@gmail.com>
2022-04-01 16:22:24 +0200

    sliders + mixers

--- app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/DrumMachineFragment.kt
+++ app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/DrumMachineFragment.kt
@@ -1,15 +1,16 @@
 package com.google.codelabs.mdc.kotlin.shrine
 
 import android.os.Bundle
-import android.os.CountDownTimer
-
 import android.util.Log
+
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.LinearLayout
 import androidx.fragment.app.Fragment
 import com.google.codelabs.mdc.kotlin.shrine.models.Beat
+import com.google.codelabs.mdc.kotlin.shrine.models.Mixer
 import com.google.codelabs.mdc.kotlin.shrine.timer.CountUpTimer
 import kotlinx.android.synthetic.main.drum_machine_fragment.view.*
 
@@ -27,25 +28,39 @@ class DrumMachineFragment : Fragment() {
         val view = inflater.inflate(R.layout.drum_machine_fragment, container, false)
 
         val beats:ArrayList<Beat> = ArrayList<Beat>()
+        val mixers:ArrayList<Mixer> = ArrayList<Mixer>()
 
-        val count:Int = view.beatsGroup.childCount
+        val beatsCount:Int = view.beatsGroup.childCount
+        val mixersCount:Int = view.mixersGroup.childCount
 
         var lastBeat: Beat? = null
 
         var beatIndex = 0
 
         // add beats from beatsGroup layout
-        for (i in 0 until count) {
+        for (i in 0 until beatsCount) {
             beats.add(Beat(view.beatsGroup.getChildAt(i) as ImageView))
         }
 
-        for (i in 0 until count) {
+        for (i in 0 until beatsCount) {
             beats[i].imageView.setOnClickListener {
                 beats[i].toggleActive()
             }
         }
 
-            fun tickCb() {
+        // add mixers from mixersGroup layout
+        for(i in 0 until mixersCount) {
+            val mixerLayout: LinearLayout = view.mixersGroup.getChildAt(i) as LinearLayout
+            mixers.add(Mixer(mixerLayout.getChildAt(1) as ImageView))
+        }
+
+        for (i in 0 until mixersCount) {
+            mixers[i].enabledButtonImageView.setOnClickListener {
+                mixers[i].toggleActive()
+            }
+        }
+
+        fun tickCb() {
             beats[beatIndex].imageView.setImageResource(R.drawable.rectangle_playing)
             if(lastBeat !== null) {
                 if(lastBeat!!.enabled) {
